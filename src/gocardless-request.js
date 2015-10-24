d5d8f9e5db68d3cf6737c1241b61a67f53aab541
Sam Coope <sam.j.coope@gmail.com>
2015-10-24 07:22:28 +0100

    Saftey commit: recursive get now working

--- src/gocardless-request.js
+++ src/gocardless-request.js
@@ -20,19 +20,50 @@ function GoCardlessRequest(authToken) {
 }
 
 // Takes a string of the endpoint desired, e.g ('customers')
-GoCardlessRequest.prototype.get = function(endpoint) {
-    return new Promise(function(fulfil, reject) {
-        this.options.uri = BASE_URL + '/' + endpoint;
-        request(this.options, function(error, repsonse, body) {
-            if (error) {
-                reject(error);
-            }
-            // something here about the response contains metadata, and i want to remove that
-            var response = JSON.parse(body)[endpoint];
-            fulfil(response);
-        });
-    }.bind(this));
+GoCardlessRequest.prototype.getAll = function(endpoint) {
+    var self = this;
+    // return new Promise(function(fulfil, reject) {
+    //     self.options.uri = BASE_URL + '/' + endpoint;
+    //     request(self.options, function(error, response, body) {
+    //         if (error) {
+    //             reject(error);
+    //         }
+    //         // something here about the response contains metadata, and i want to remove that
+    //
+    //         var responseBody = JSON.parse(body);
+    //         console.log(responseBody);
+    //         var data = responseBody[endpoint];
+    //
+    //         fulfil(data);
+    //     });
+    // });
+    return getRecursive(BASE_URL + '/' + endpoint, []);
 
+    function getRecursive(url, data, after) {
+        return new Promise(function(fulfil, reject) {
+            self.options.uri = after ? url + '?after=' + after : url;
+            request(self.options, function(error, response, body) {
+                if (error) {
+                    reject(error);
+                }
+                // something here about the response contains metadata, and i want to remove that
+
+                var responseBody = JSON.parse(body);
+                var responseData = responseBody[endpoint];
+                data = data.concat(responseData);
+
+                // console.log('body after ' + after + ': ', responseBody);
+
+                // TODO we need to check what the hell this is meant to be
+                var nextID = responseBody.meta.cursors.after;
+                if (nextID) {
+                    fulfil(getRecursive(url, data, nextID));
+                } else {
+                    fulfil(data);
+                }
+            });
+        });
+    }
 };
 
 module.exports = GoCardlessRequest;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-var GoCardlessRequest = require('./gocardless-request');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
+
+var GoCardlessRequest = require('./gocardless-request');
+var paymentResolver = require('./payment-resolver');
+var csvWriter = require('./csv-writer');
 
 var args = process.argv.slice(2);
 
@@ -12,20 +15,23 @@ if (!token) {
 
 var gocardlessRequest = new GoCardlessRequest(token);
 
-// Get all of the payments, customers and mandates in the server
-Promise.props({
-    customers: gocardlessRequest.get('customers'),
-    payments: gocardlessRequest.get('payments'),
-    mandates: gocardlessRequest.get('mandates')
-}).then(function(responses) {
-    var customers = responses.customers;
-    var payments = responses.payments;
-    var mandates = responses.mandates;
-    console.log(mandates);
-
-});
+// // Get all of the payments, customers and mandates in the server
+// Promise.props({
+//     customers: gocardlessRequest.get('customers'),
+//     payments: gocardlessRequest.get('payments'),
+//     mandates: gocardlessRequest.get('mandates')
+// }).then(function(responses) {
+//     var customers = responses.customers;
+//     var payments = responses.payments;
+//     var mandates = responses.mandates;
+//     console.log(payments.length);
 //
-// betterClient.request('/payments')
-// .then(function(payments) {
-//     console.log(payments);
+//     var data = paymentResolver.resolvePayments(payments, customers, mandates);
+//     // console.log(csvWriter.toCSV(data));
+//
 // });
+
+gocardlessRequest.getAll('customers')
+.then(function(customers) {
+    console.log(JSON.stringify(customers));
+});
